52c2cde0cc6371d82ed7a3e07b5257c542f5b761
Julian Whitley <jln.whitley@gmail.com>
2025-01-26 13:13:20 -0700

    added pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPwa = require("next-pwa")({
+  dest: "public",
+  swSrc: "/public/custom-service-worker.js",
+  register: true,
+  skipWaiting: true,
+  buildExcludes: [/middleware-manifest.json$/, /app-build-manifest.json$/],
+  disable: process.env.NODE_ENV === "development",
+});
+
+
+const nextConfig = withPwa({
   reactStrictMode: true,
   typescript: {
     // !! WARN !!
@@ -13,7 +23,25 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  // Add any other Next.js config options here
-}
+});
 
-export default nextConfig;
+module.exports = nextConfig;
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   typescript: {
+//     // !! WARN !!
+//     // Dangerously allow production builds to successfully complete even if
+//     // your project has type errors.
+//     // !! WARN !!
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+//   // Add any other Next.js config options here
+// }
+
+// export default nextConfig;
